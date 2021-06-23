6a5d5e2716802b42894d2b0dc2e40a45cfa2a444
Vishwa.R <64256342+code-reaper08@users.noreply.github.com>
2021-06-24 00:02:58 +0530

    Added content in middle section.
    Added additional stylings and provided links too

--- Middle.js
+++ Middle.js
@@ -1,16 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const Middle = () => {
-    return (
-        <div>
-            <p className="Mid">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel est quibusdam, provident sint ratione pariatur in facere atque facilis labore voluptas exercitationem officia repellat aliquid amet delectus consectetur quos vero?
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa distinctio id a veritatis, magni nobis at voluptas. Excepturi natus consequuntur, perspiciatis porro temporibus ducimus iste praesentium sequi cum suscipit assumenda.
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore dolorem id suscipit maiores sint ea labore dolorum? Perspiciatis recusandae quia, sunt omnis fuga, quis quisquam ab, aliquam illo saepe provident.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus provident voluptate quas consectetur exercitationem. At mollitia non voluptatum deleniti excepturi totam possimus asperiores quas expedita aut, nihil, itaque saepe porro.
-            </p>
-        </div>
-    )
-}
+  return (
+    <div>
+      <p id="mid">
+        A curious learner of Technology ⚙. Python | java | c | c++ | javascript
+        A coder always with his functions open 🔓. Love❤️ doing projects with
+        what I Learnt. Other than these, I love to play football ⚽,to paint 🎨,
+        Photoshoppping 📷 and to read books📚. I am a project driven individual
+        , a lifetime learner and a student currently pursuing my 2nd year of
+        engineering degree in the field of Computer science 💻 in <a id="cllglink" href="https://www.sathyabama.ac.in/" target="_blank">Sathyabama
+        Institute of Science and Technology</a>. I love to explore things, to meet
+        new people and to work forward for the welfare of the society and the
+        human community. I am a continuous
+        learner and explorer who love to dive deep into the Javascript Ocean. A
+        GitHub Developer Program Member With no wonder, I am a UnderGrad
+        pursuing Computer Science and Engineering as my major field of study in
+        India. Guess these below points would fit me in, I am a Student.linkedin
+        I love to play and watch Football.⚽ I love programming.💻 I feel
+        rebel.🐱‍🏍 I vibe to 2pac, Bob Marley and freddie.🎶
+      </p>
+    </div>
+  );
+};
 
-export default Middle
-   
+export default Middle;
