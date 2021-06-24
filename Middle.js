9cea00ea4a20e0ae62a24a8ba71b5fcf7d5b1555
Vishwa.R <64256342+code-reaper08@users.noreply.github.com>
2021-06-24 15:09:44 +0530

    Added all contents in homepage with avatar

--- Middle.js
+++ Middle.js
@@ -1,26 +1,30 @@
 import React from "react";
-
+import lbd1 from "../assets/bord.png";
 const Middle = () => {
   return (
-    <div>
-      <p id="mid">
-        A curious learner of Technology ⚙. Python | java | c | c++ | javascript
-        A coder always with his functions open 🔓. Love❤️ doing projects with
-        what I Learnt. Other than these, I love to play football ⚽,to paint 🎨,
-        Photoshoppping 📷 and to read books📚. I am a project driven individual
-        , a lifetime learner and a student currently pursuing my 2nd year of
-        engineering degree in the field of Computer science 💻 in <a id="cllglink" href="https://www.sathyabama.ac.in/" target="_blank">Sathyabama
-        Institute of Science and Technology</a>. I love to explore things, to meet
+          
+      <div className="container">
+        {/* <div>
+          <img src={lbd1} alt="" />
+        </div> */}
+        <p id="mid">
+       I am <strong>Vishwa</strong>, A Polyglot Developer, OpenSource adherent, Who talks in JavaScript, Python and Java mostly. I love those tags in HTML, and love to paint them in CSS (BTW I am a Artist too, by <strong>ART</strong> I mean the Traditional one.). I am currently a sophomore pursuing major in Computer Science and Engineering @ <a id="cllglink" href="https://www.sathyabama.ac.in/" target="_blank">Sathyabama
+        Institute of Science and Technology</a>. 
+        <br />
+        <br />
+        I love to explore things, to meet
         new people and to work forward for the welfare of the society and the
         human community. I am a continuous
         learner and explorer who love to dive deep into the Javascript Ocean. A
-        GitHub Developer Program Member With no wonder, I am a UnderGrad
-        pursuing Computer Science and Engineering as my major field of study in
-        India. Guess these below points would fit me in, I am a Student.linkedin
-        I love to play and watch Football.⚽ I love programming.💻 I feel
-        rebel.🐱‍🏍 I vibe to 2pac, Bob Marley and freddie.🎶
-      </p>
-    </div>
+        GitHub Developer Program Member With no wonder.
+        <br />
+        <br />
+        I love to play and watch Football.⚽ <br />
+        I love programming.💻 <br />
+        I feel rebel.🐱‍🏍 <br />
+        I vibe to 2pac, Bob Marley and freddie.🎶
+        </p>
+      </div>
   );
 };
 
